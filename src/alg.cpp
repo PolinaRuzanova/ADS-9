c6248cb0a121a46a5000026f4d995725570d9d97
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-05-29 23:06:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,23 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> derevo;
+    std::string slovo = "";
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return derevo;
+    }
+    while (!file.eof()) {
+        char sym = file.get();
+        if ((sym >= 'a' && sym <= 'z') || (sym >= 'A' && sym <= 'Z')) {
+            slovo += tolower(sym);
+        }
+        else if (slovo != "") {
+            derevo.add(slovo);
+            slovo = "";
+        }
+    }
+    file.close();
+    return derevo;
 }
